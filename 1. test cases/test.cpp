e16750489d1c5c3a351a49c74571b32e87903655
janhviKumbharkar <86514109+janhviKumbharkar@users.noreply.github.com>
2022-09-06 11:50:12 +0530

    Add files via upload

---
+++ 1. test cases/test.cpp
@@ -0,0 +1,16 @@
+// jb btana ho total kitne inputs lene h
+// code will run until t becomes 0;
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) // while(t!=0)
+    {
+
+        // code
+    }
+    return 0;
+}
