79e451994ab17f772f269c6c6d9e8787cf7bfe4e
janhviKumbharkar <86514109+janhviKumbharkar@users.noreply.github.com>
2022-10-11 20:16:17 +0530

    Add files via upload

---
+++ compare.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=4;
+    int b=5;
+
+if(a>b){
+    cout<<"a is greater than b";
+}
+else{
+    cout<<"b is greater than a";
+}
+return 0;
+}
